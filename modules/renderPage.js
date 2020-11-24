2903e30449e02fd18343d52408e7640f14740e62
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2020-11-24 14:00:08 +0200

    start app

---
+++ modules/renderPage.js
@@ -0,0 +1,7 @@
+let renderPage = (req, res, err) => {
+    res.render(`main`, {});
+}
+
+module.exports = {
+    renderPage
+};
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const renderPage = require('./modules/renderPage');
+
+
+
+
+// let passport = require('passport'); 
+// let FacebookStrategy = require('passport-facebook').Strategy;
+// let GoogleStrategy = require('passport-google-oauth20').Strategy;
+// let GitHubStrategy = require('passport-github').Strategy;
+
+
+
+// passport.serializeUser(function(user, done) {done(null, user)});  
+// passport.deserializeUser(function(obj, done) {done(null, obj)});
+// app.use(passport.initialize());
+
+
+// Facebook Strategy
+// passport.use(new FacebookStrategy({
+//     clientID: '435548787037664',
+//     clientSecret: '1a2fde88089878abfa800a93a0fccbd0',
+//     callbackURL: "https://svyrydvladymyr.herokuapp.com/facebookcallback",
+//     profileFields: ['id', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'picture.type(large)']
+// }, function(accessToken, refreshToken, profile, done) {process.nextTick(function () {autorisationSocial(profile, done)})}));
+// app.get('/facebook', passport.authenticate('facebook'));
+// app.get('/facebookcallback', function(req, res, next) {passport.authenticate('facebook', function(err, user, info) {autorisRouts(req, res, err, user)})(req, res, next)});
+
+// Google Strategy
+// passport.use(new GoogleStrategy({
+//     clientID: '422337007127-73a8ofjsl3a6n7kesl3tnk0c11jo4ou6.apps.googleusercontent.com',
+//     clientSecret: '1J24VS_1Yrk27ZZCgs5NMk60',
+//     callbackURL: "https://svyrydvladymyr.herokuapp.com/googlecallback",
+//     profileFields: ['id', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'picture.type(large)']
+//   },  function(accessToken, refreshToken, profile, done) {process.nextTick(function () {autorisationSocial(profile, done)})}));
+// app.get('/google', passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']}));
+// app.get('/googlecallback', function(req, res, next) {passport.authenticate('google', function(err, user, info) {autorisRouts(req, res, err, user)})(req, res, next)});
+
+// GitHub Strategy
+// passport.use(new GitHubStrategy({
+//     clientID: '306581483c8927ca31b4',
+//     clientSecret: '13853ceb1fc550a6027df1efee453a9f941fd683',
+//     callbackURL: "https://svyrydvladymyr.herokuapp.com/githubcallback",
+//     profileFields: ['id', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'picture.type(large)']
+//   },  function(accessToken, refreshToken, profile, done) {process.nextTick(function () {autorisationSocial(profile, done)})}));
+// app.get('/github', passport.authenticate('github'));
+// app.get('/githubcallback', function(req, res, next) {passport.authenticate('github', function(err, user, info) {autorisRouts(req, res, err, user)})(req, res, next)});
+
+
+
+
+
+
+//template engineer
+app.set('views', __dirname + '/templates'); 
+app.set('view engine', 'ejs');
+
+//static files
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+//user pages
+// app.use('/:pageid$', (req, res) => {renderPage(req, res)});
+app.get('/$', (req, res, next) => {res.redirect('main')});
+app.get('*', (req, res) => {res.status(404).send(`<p style="text-align: center; color: red; margin: 200px auto; font: bold 30px Arial;">PAGE NOT FOUND!!!</p>`);});
+
+app.listen(process.env.PORT || 4000, () => {console.log('Server is running...')});
