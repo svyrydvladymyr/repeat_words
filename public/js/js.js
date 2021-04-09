cf6061764b5c8d46bddf610d684cc545b88446ab
Svyryd Vladymyr <svyrydvladimir@gmail.com>
2021-04-09 15:16:54 +0300

    finish animation for settings menu

--- public/js/js.js
+++ public/js/js.js
@@ -11,20 +11,42 @@
 
 
     // add alert message row
-    if ($_('.main_body').length > 0) {
-        console.log($_('.main_body')[0]);
-        $_('.main_body')[0].innerHTML += '<p id="alert-message"></p> '
-        
-    }
+    if ($_('.main_body').length > 0) { $_('.main_body')[0].innerHTML += '<p id="alert-message"></p>' };
 
     // dropdown menu
-    const showSettingsList = () => { $_('.user_settings_list')[0].classList.toggle('user_settings_list_show')};
-    window.onclick = function(event) {
+    const dropdowns = $_('.user_settings_list');
+    const makeCounter = function() {
+        let privateCounter = 0;
+        const changeBy = val => privateCounter += val;
+        return {
+            increment: () => changeBy(1), 
+            decrement: () => changeBy(-1), 
+            value: () => privateCounter
+        };
+    };
+    const counter = makeCounter();    
+    const menuAnimation = (change) => {
+        let animation = setInterval(() => { 
+            let border = change === 'decrement' ? 0 : 10;           
+            if (change === 'decrement') { counter.decrement() };
+            if (change === 'increment') { counter.increment() };          
+            let count = counter.value();
+            dropdowns[0].style.minWidth = `${20 + count*10}px`;
+            dropdowns[0].style.opacity = `${0 + count/10}`;
+            dropdowns[0].style.fontSize = `${count + 3}px`;
+            if (count === border) { clearInterval(animation) };
+        }, 20); 
+    };
+    const showSettingsList = () => { 
+        dropdowns[0].classList.toggle('user_settings_list_show');
+        dropdowns[0].classList.contains('user_settings_list_show') ? menuAnimation('increment') : menuAnimation('decrement');
+    };    
+    window.onclick = function(event) {        
         if (!event.target.matches('.user_settings_list_wrap > i')) {
-          const dropdowns = $_('.user_settings_list');
             for (let i = 0; i < dropdowns.length; i++) {
                 if (dropdowns[i].classList.contains('user_settings_list_show')) {
-                    dropdowns[i].classList.remove('user_settings_list_show');
+                    setTimeout(() => { dropdowns[i].classList.remove('user_settings_list_show') }, 200);
+                    menuAnimation('decrement');
                 };
             };
         };
